a81d112514d250154c10f1a01c6898534355384b
makiwakurin <takahiro1608@gmail.com>
2020-10-28 10:32:02 +0900

    frstcommit

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :user do
     transient do
       person { Gimei.name }
-   end
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
+    end
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
     birthday { Faker::Date.backward }
     first_name { person.first.kanji }
     last_name { person.last.kanji }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,44 +6,44 @@ describe User do
 
   describe 'ユーザー新規登録' do
     context '新規登録がうまくいくとき' do
-      it "nicknameとemail、password, first_name,last_name,first_name_kana,last_name_kana,birthdayが存在すれば登録できる" do
+      it 'nicknameとemail、password, first_name,last_name,first_name_kana,last_name_kana,birthdayが存在すれば登録できる' do
         expect(@user).to be_valid
       end
-      it "passwordが6文字以上であれば登録できる" do
+      it 'passwordが6文字以上であれば登録できる' do
         expect(@user).to be_valid
       end
-      it "paswordが半角英数字混合であれば登録できる" do
+      it 'paswordが半角英数字混合であれば登録できる' do
         expect(@user).to be_valid
       end
     end
 
     context '新規登録がうまくいかないとき' do
-      it "nicknameとemail、password, first_name,last_name,first_name_kana,last_name_kana,birthdayが空だと登録できない" do
-        @user.nickname = ""
-        @user.email = ""
-        @user.password =""
-        @user.first_name= ""
-        @user.last_name = ""
-        @user.first_name_kana = ""
-        @user.last_name_kana = ""
-        @user.birthday = ""
+      it 'nicknameとemail、password, first_name,last_name,first_name_kana,last_name_kana,birthdayが空だと登録できない' do
+        @user.nickname = ''
+        @user.email = ''
+        @user.password = ''
+        @user.first_name = ''
+        @user.last_name = ''
+        @user.first_name_kana = ''
+        @user.last_name_kana = ''
+        @user.birthday = ''
         @user.valid?
         expect(@user.errors.full_messages)
       end
-      it "重複したemailが存在する場合登録できない" do
+      it '重複したemailが存在する場合登録できない' do
         @user.save
         another_user = FactoryBot.build(:user)
         another_user.email = @user.email
         another_user.valid?
         expect(another_user.errors.full_messages)
       end
-      it "passwordが5文字以下であれば登録できない" do
-        @user.password = "00000"
-        @user.password_confirmation = "00000"
+      it 'passwordが5文字以下であれば登録できない' do
+        @user.password = '00000'
+        @user.password_confirmation = '00000'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
     end
   end
 end
-# bundle exec rspec spec/models/user_spec.rb 
+# bundle exec rspec spec/models/user_spec.rb
