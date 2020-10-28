d42d6de17972c51aa4947d94ce53f1095634a0f9
makiwakurin <takahiro1608@gmail.com>
2020-10-28 09:51:44 +0900

    gem gimeiの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :email,:first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true   
-  validates :email, uniqueness: true  
+  validates :nickname, :email,:first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true    
   validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "ユーザー本名は全角（漢字・ひらがな・カタカナ）で入力してください"}
   validates :first_name_kana, :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "ユーザー本名のフリガナは全角（カタカナ）で入力してください"}
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: "半角英数字混合で入力して下さい"}
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,16 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
-    password              {"00000000"}
+    transient do
+      person { Gimei.name }
+   end
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    birthday { Faker::Date.backward }
+    first_name { person.first.kanji }
+    last_name { person.last.kanji }
+    first_name_kana { person.first.katakana }
+    last_name_kana { person.last.katakana }
   end
 end
