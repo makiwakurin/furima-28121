0c525b64266310c2e5afb1baffd3ce23c279520e
makiwakurin <takahiro1608@gmail.com>
2020-10-30 23:50:01 +0900

    statusモデル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,25 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
-    
+   @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :text, :category_id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+  validates :name, :text, :category, presence: true
+  validates :category_id, numericality: { other_than: 1 }
 end
-                                      
