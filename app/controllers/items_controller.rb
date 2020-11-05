865ed1bdfd82af63e908d1bc4d21f214f4606d5d
makiwakurin <takahiro1608@gmail.com>
2020-11-05 18:58:10 +0900

    コメントアウト削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,7 +22,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    # binding.pry
     if user_signed_in? && current_user.id == @item.user_id
       @item.destroy
       redirect_to root_path
