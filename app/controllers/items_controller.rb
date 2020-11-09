2cb417f0c32469dd15d54210401b458fabff1715
makiwakurin <takahiro1608@gmail.com>
2020-11-09 14:54:52 +0900

    authenticate_user!メソッドをitemsコントローラーのbeforeアクション内へ移動

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :destroy, :edit, :update]
-
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
