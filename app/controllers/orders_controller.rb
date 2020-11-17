d0abec14a585406b0c94febef19c558b2949a4e2
makiwakurin <takahiro1608@gmail.com>
2020-11-17 11:11:35 +0900

    ordersコントローラへ before_action :authenticate_user!を定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,22 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @item = Item.find(params[:item_id])
   end
 
   def create
-    
+    # binding.pry
+    @item = Item.new(order_params)
+    if @item.save
+      redirect_to :index
+    end
   end
 
+  private
+
+  def order_params
+    params.require(:item).permit(item_id: item.id, user_id: user.id)
+  end
 end
+# , :postal_coad, :region_id, :municipality, :house_number, :building_name, :phone_number
