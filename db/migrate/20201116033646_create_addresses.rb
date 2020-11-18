e87484d1080640ca21d2cd5aa04570e96b87872d
makiwakurin <takahiro1608@gmail.com>
2020-11-18 14:18:44 +0900

    addressesテーブルのphone_numberの型をstringへ変更

--- db/migrate/20201116033646_create_addresses.rb
+++ db/migrate/20201116033646_create_addresses.rb
@@ -6,7 +6,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.string     :municipality,               null: false
       t.string     :house_number,               null: false
       t.string     :building_name             
-      t.integer    :phone_number ,              null: false
+      t.string     :phone_number ,              null: false
       # t.references :order,                      null: false, foreign_key: true 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_033646) do
     t.string "municipality", null: false
     t.string "house_number", null: false
     t.string "building_name"
-    t.integer "phone_number", null: false
+    t.string "phone_number", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
