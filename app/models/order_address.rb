c77e46a01fd937f453537a2535b2ac497cc7379e
makiwakurin <takahiro1608@gmail.com>
2020-11-19 09:49:11 +0900

    phone_numberバリデーション追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -7,8 +7,9 @@ class OrderAddress
     validates :postal_cord, :region_id, :municipality, :house_number, :phone_number
   end
 
-  validates :postal_cord, format: { with:/\A\d{3}[-]\d{4}\z/ } 
-
+  validates :postal_cord, format: { with: /\A\d{3}[-]\d{4}\z/ } 
+  validates :phone_number, format: {with: /\A\d{11}\z/}
+  
   def save
     Order.create(item_id: item_id, user_id: user_id)
     Address.create(postal_cord: postal_cord, region_id: region_id, municipality: municipality, house_number: house_number, building_name: building_name, phone_number: phone_number)
