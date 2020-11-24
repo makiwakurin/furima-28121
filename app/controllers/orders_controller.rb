0c5e8c3f54dec9b39b9c53a39028eb00d5e38215
makiwakurin <takahiro1608@gmail.com>
2020-11-24 11:45:16 +0900

    トークン情報

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,43 @@
 class OrdersController < ApplicationController
 
   def index
+<<<<<<< Updated upstream
   end
   
+=======
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    # binding.pry
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_cord, :region_id, :municipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], order_id: params[:order_id], token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_d84a4a3a2cef459cb6b729f9"
+    Payjp::Charge.create(
+      amount: @item.price ,
+      card: order_params[:token],
+      currency: 'jpy'   
+    )
+  end
+
+>>>>>>> Stashed changes
 end
