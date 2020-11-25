f95221b8f3559b48d2df2cd45e8a877f6d458689
makiwakurin <takahiro1608@gmail.com>
2020-11-25 11:12:20 +0900

    購入済み商品ページへの　url直うち禁止処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index]
 
   def index
     @order_address = OrderAddress.new
@@ -34,6 +35,12 @@ class OrdersController < ApplicationController
       card: order_params[:token],
       currency: 'jpy'   
     )
+  end
+
+  def move_to_index
+   unless @item.order == nil
+    redirect_to root_path
+   end
   end
 
 end
